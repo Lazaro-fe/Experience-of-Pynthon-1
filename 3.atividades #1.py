17f9e30c18c07983b82cea3b5b70817323254ff3
Lazaro-fe <155861352+Lazaro-fe@users.noreply.github.com>
2025-02-13 19:57:11 +0000

    Salvando arquivos.

---
+++ 3.atividades #1.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("clear") # Limpa o Terminal
+
+# Solicitando os dados 
+numero = int(input("Digite um número : "))
+
+# Verificando (Processamento)
+if numero < 10
+    print()
+
+# Exibindo dados (Saída)
