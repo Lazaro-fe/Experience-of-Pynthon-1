b5259a50506b3cfbe4ff60b01f6736710145dfe0
Lazaro-fe <155861352+Lazaro-fe@users.noreply.github.com>
2025-02-21 19:57:52 +0000

    Salvar

---
+++ algebra-booleana/menu.py
@@ -0,0 +1,19 @@
+#   Limpando o Terminal
+import os
+os.system("clear")
+
+#   Solicitando dados ao usuário
+print("""
+================ MENU ============
+Código \tPrato   \t\tValor
+1   \t\tPicanha   \t\t25,00
+2   \t\tLasanha    \t\t20,00
+3   \t\tStrogonoff  \t\t18,00
+4   \t\tBife acebolado \t\t15,00
+5   \t\tPão com ovo     \t5,00
+""")
+
+opcao = int(input("Digite a sua opcão : "))
+
+#   Verificando
+
