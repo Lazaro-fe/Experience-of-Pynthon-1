621a0f9a6be23619f2a3e3909dab390807e4c95f
Lazaro-fe <155861352+Lazaro-fe@users.noreply.github.com>
2025-02-21 19:04:02 +0000

    Salvar

---
+++ algebra-booleana/maior_menor-numero-ex2.py
@@ -0,0 +1,8 @@
+# Limpando o Terminal
+import os
+os.system("clear")
+
+# Solicitando o maior e o menor
+primeiro_numero = int(input("Digite o primeiro número: "))
+segundo_numero = int(input("Digite o segundo número: "))
+terceiro_numero = int(input("Digite o terceiro número: "))
