6f81d894cfc31fa7da772f5e9e8f749a144ac95e
Lazaro-fe <155861352+Lazaro-fe@users.noreply.github.com>
2025-03-02 15:02:45 +0000

    Salvando

--- atividade-pagamento.py
+++ atividade-pagamento.py
@@ -1,6 +1,7 @@
 # Limpeza do Terminal
 import os
 os.system("clear")
+os.system('cls'if os.name == 'nt' else 'clear')
 
 # Solicitando dados ao Usuário
 
@@ -12,26 +13,27 @@ print("""
                 2-Parcelado
 """)
 print()
-
 pagamento = int(input("Digite sua forma de pagamento : "))
 
+os.system('cls'if os.name == 'nt' else 'clear')
 # Verficando 
 match pagamento:
     case 1:
         # Obtendo valor do desconto de 10%
         print()
         valor_inicial = valor_do_produto_comprado
-        desconto = valor_do_produto_comprado * 0,10
+        desconto = valor_do_produto_comprado * 10
         print(f"Valor obtido : {desconto}")
         print(f"Valor do Produto : {valor_inicial}")
+        print(f"Valor do desconto : {0,10}")
     case 2:
         print()
         parcelas = int(input("Digite a quantidade de parcelas que o você quer :"))
         valor_da_parcela = valor_do_produto_comprado / parcelas
         valor_inicial2 = valor_do_produto_comprado
-        print(f"Valor obtido : {valor_da_parcela}")
+        print(f"Valor das parcelas : {valor_da_parcela}")
         print(f"Valor do Produto : {valor_do_produto_comprado}")
-        print(f"Valor da Parcela : {parcelas}")
+        print(f"Quantidade de Parcelas : {parcelas}")
     case _:
         print()
         print("====OPERAÇÃO INVÁLIDA====")
