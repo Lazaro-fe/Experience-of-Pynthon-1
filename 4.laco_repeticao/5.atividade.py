63e2c78803d6dd0dc00db2bd563c193f1a06528f
Lázaro <155861352+Lazaro-fe@users.noreply.github.com>
2025-05-27 17:07:05 -0300

    salvar

--- 4.laco_repeticao/5.atividade.py
+++
@@ -1,9 +0,0 @@
-# Limpeza de Terminal
-import os
-import time
-os.system("clls || clear")
-
-numero = 0
-for i in range(1,6):
-    numero += int(input("Digite um número : "))
-    print(f"Soma : {numero}")
