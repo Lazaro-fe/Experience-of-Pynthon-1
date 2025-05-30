01b3b958183ff9defe3ec05bac8d697162cb719a
Lázaro <155861352+Lazaro-fe@users.noreply.github.com>
2025-05-30 16:53:24 -0300

    Add files via upload

---
+++ Notas.js
@@ -0,0 +1,9 @@
+const readline = require('readline-sync')
+console.clear()
+
+let n1 = readline.questionInt('Digite a nota do teste : ')
+let n2 = readline.questionInt('Digite a nota da Prova : ')
+let n3 = readline.questionInt('Digite a nota do qualitativo : ')
+console.clear()
+
+soma
